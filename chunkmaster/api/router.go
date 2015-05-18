e63b607b57d2c26ec0d132574842aff4b2db4480
zhangcoldwater <zhangcoldwater@163.com>
2015-05-18 22:00:52 +0800

    delete useless code

--- chunkmaster/api/router.go
+++ chunkmaster/api/router.go
@@ -9,10 +9,7 @@ var RouteMap map[string]map[string]http.HandlerFunc
 func init() {
 	RouteMap = map[string]map[string]http.HandlerFunc {
 		"POST": {
-			//"/v1/chunkserver/push"  : chunkserverPushHandler,
 			"/v1/chunkserver/initserver" : initChunkserverHandler,
-			//for monitor
-			//"/v1/chunkserver/alarm" : monitorReportHandler,
 			"/v1/chunkserver/reloadinfo" : loadChunkserverInfoHandler,
 			"/v1/chunkserver/reportinfo" : reportChunkserverInfoHandler,
 		},
