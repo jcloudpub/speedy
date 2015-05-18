b81b5da7e9cedbb54f4215a040bda6b39d8cd631
zhangcoldwater <zhangcoldwater@163.com>
2015-05-18 22:02:16 +0800

    add a flag to choose log level

--- cmd/imageserver/main.go
+++ cmd/imageserver/main.go
@@ -12,15 +12,19 @@ import (
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
 
-	os.Setenv("DEBUG", "DEBUG")
 	var masterAddr = flag.String("mh", "127.0.0.1", "chunkmaster addr")
 	var masterPort = flag.Int("mp", 8099, "chunkmaster port")
 	var host = flag.String("h", "0.0.0.0", "imageserver listen ip")
 	var port = flag.Int("p", 6788, "imageserver listen port")
 	var limitNum = flag.Int("n", 2, "the limit num of available chunkserver each chunkserver group")
 	var metaHost = flag.String("meta", "127.0.0.1;3306;root;;metadb", "meta server config eg. for mysql meta host;port;user;passwd;db")
+	var debug = flag.Bool("D", false, "log debug level")
 
 	flag.Parse()
+
+	if *debug {
+		os.Setenv("DEBUG", "DEBUG")
+	}
 
 	var masterUrl = "http://" + *masterAddr + ":" + strconv.Itoa(*masterPort)
 	var imageServerAddr = *host + ":" + strconv.Itoa(*port)
