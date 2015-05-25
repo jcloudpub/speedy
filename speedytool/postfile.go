a3847500c597e804198334799c0ed248ae890c13
zhangcoldwater <zhangcoldwater@163.com>
2015-05-25 19:10:32 +0800

    suport delete meta info

--- speedytool/postfile.go
+++ speedytool/postfile.go
@@ -17,37 +17,56 @@ const (
 	PATH_PREFIX     = "loadtest/abc/key_"
 )
 
-func PostFileTestSpeedyConcurrency(imageserverAddr string, fileName string, numGoroutine int, partSize int) {
-	log.Infof("[PostFileTestSpeedyConcurrency] ==== begin")
+func preprocessFile(fileName string, partSize int) (fileBody []byte, fileSize int64, partCount int, partial int, err error) {
 	file, err := os.Open(fileName)
 	if err != nil {
-		log.Errorf("[PostFileTestSpeedyConcurrency] open file %s err: %v", fileName, err)
+		log.Errorf("[preprocessFile] open file %s err: %v", fileName, err)
 		return
 	}
 	defer file.Close()
 
 	info, err := file.Stat()
 	if err != nil {
-		log.Errorf("[PostFileTestSpeedyConcurrency] get file %s stat err: %v", fileName, err)
+		log.Errorf("[preprocessFile] get file %s stat err: %v", fileName, err)
+		return nil, 0, 0, 0, fmt.Errorf("[preprocessFile] get file %s  stat err: %v", fileName, err)
+	}
+
+	fileSize = info.Size()
+	log.Infof("file %s size: %v", fileName, fileSize)
+	fileBody = make([]byte, fileSize)
+	nread, err := file.Read(fileBody)
+	if err != nil || int64(nread) != fileSize {
+		log.Errorf("[preprocessFile] read %s nread: %v, fileSize: %v, err: %v", fileName, nread, fileSize, err)
+		return nil, 0, 0, 0, fmt.Errorf("[preprocessFile] read %s nread: %v, fileSize: %v, err: %v", fileName, nread, fileSize, err)
+	}
+
+	partCount = int(fileSize/int64(partSize))
+	partial = int(fileSize%int64(partSize))
+	return fileBody, fileSize, partCount, partial, nil
+}
+
+func TestSpeedyConcurrency(imageserverAddr string, fileName string, numGoroutine int, partSize int) {
+	log.Infof("[TestSpeedyConcurrency] ==== begin")
+
+	fileBody, fileSize, partCount, partial, err := preprocessFile(fileName, partSize)
+	if err != nil {
+		log.Errorf("[TestSpeedyConcurrency] preprocessFile error: %v", err)
 		return
 	}
 
-	fileSize := info.Size()
-	log.Infof("file %s size: %v", fileName, fileSize)
-	fileBody := make([]byte, fileSize)
-	nread, err := file.Read(fileBody)
-	if err != nil || int64(nread) != fileSize {
-		log.Errorf("[PostFileTestSpeedyConcurrency] read %s nread:%v, fileSize:%v, err: %v", fileName, nread, fileSize, err)
-		return
-	} 
+	postFileTestSpeedyConcurrency(imageserverAddr, fileBody, numGoroutine, fileSize, partCount, partial, partSize)
 
-	partCount := int(fileSize/int64(partSize))
-	partial := int(fileSize%int64(partSize))
-	result := make([]int, numGoroutine)
+	log.Infof("[PostFileTestSpeedyConcurrency] delete file === begin")
+	deleteFileTestSpeedyConcurrency(imageserverAddr, numGoroutine, partCount, partial)
+	log.Infof("[PostFileTestSpeedyConcurrency] delete file === end")
+	log.Infof("[TestSpeedyConcurrency] ==== end")
+}
+
+func postFileTestSpeedyConcurrency(imageserverAddr string, fileBody []byte, numGoroutine int, fileSize int64, partCount int, partial int, partSize int) {
 	var wg sync.WaitGroup
 	wg.Add(numGoroutine)
 	begin := time.Now()
-
+	result := make([]int, numGoroutine)
 	for i := 0; i < numGoroutine; i++ {
 		go postFileTestSpeedy(imageserverAddr, i, fileBody, partCount, partial, partSize, result, &wg)
 	}
@@ -58,19 +77,45 @@ func PostFileTestSpeedyConcurrency(imageserverAddr string, fileName string, numG
 	success := true 
 	for i := 0; i < numGoroutine; i++ {
 		if result[i] != 1 {
-			log.Errorf("result[%d] error", i)
+			log.Errorf("[analysizePostFile] post error, goroutine %d error", i)
 			success = false
 		}
 	}
 
 	if !success {
-		log.Infof("[PostFileTestSpeedyConcurrency] error")
+		log.Infof("[analysizePostFile] error")
 		return
 	}
 	totalSize := fileSize * int64(numGoroutine)
 	bandWidth := (float64(totalSize)/duration.Seconds())/(1024*1024) //B to MB
 	log.Infof("[PostFileTestSpeedyConcurrency] upload bandWidth: %v MB/s", bandWidth)
 }
+
+func deleteFileTestSpeedyConcurrency(imageserverAddr string, numGoroutine int, partCount int, partial int) {
+	log.Infof("[DeleteFileTestSpeedyConcurrency] ==== begin")
+	var wg sync.WaitGroup
+	wg.Add(numGoroutine)
+	for i := 0; i < numGoroutine; i++ {
+		go deleteFileTestSpeedy(imageserverAddr, i, partCount, partial, &wg)
+	}
+	wg.Wait()
+	log.Infof("[DeleteFileTestSpeedyConcurrency] ==== end")
+}
+
+func deleteFileTestSpeedy(imageserverAddr string, index int, partCount int, partial int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	if partCount == 0 && partial == 0 {
+		return
+	}
+	path := PATH_PREFIX_DIR + strconv.Itoa(index)
+	err := deleteFile(imageserverAddr, path)
+	if err != nil {
+		log.Errorf("[deleteFileTestSpeedy] error: %v", err)
+		return
+	}
+	log.Infof("[deleteFileTestSpeedy] %s success", path)
+}
+
 
 func postFileTestSpeedy(imageserverAddr string, index int, fileBody []byte, partCount int, partial int, partSize int, result []int, wg *sync.WaitGroup) {
 	defer wg.Done()
@@ -124,9 +169,19 @@ func postFile(imageserverAddr string, path string, data []byte, index int,  begi
 
 	_, statusCode, err := util.Call("POST", imageserverAddr, "/v1/file", bytes.NewBuffer(data), header )
 	if err != nil || statusCode != http.StatusOK {
-		return fmt.Errorf("[postFile] failed, path:%s, error:%v, statusCode:%v", path, err, statusCode)
+		return fmt.Errorf("[postFile] failed, path: %s, error: %v, statusCode: %v", path, err, statusCode)
 	}
-	log.Infof("[postFile] success, index:%v, path:%s", index, path)
+	log.Infof("[postFile] success, index: %v, path: %s", index, path)
 	return nil
 }
 
+func deleteFile(imageserverAddr string, path string) error {
+	header := make(map[string][]string)
+	header["Path"] = []string{path}
+	_, statusCode, err := util.Call("DELETE", imageserverAddr, "/v1/file", nil, header)
+	if err != nil || statusCode != http.StatusNoContent {
+		return fmt.Errorf("[deleteFile] failed, path: %s, error: %v, statusCode: %v", path, err, statusCode)
+	}
+	log.Infof("[deleteFile] success")
+	return nil
+}
